04a5d9ac659c915e865ac9e2346366900a8ad3d0
mohamedDVP <mohamed.mak@laposte.net>
2024-06-02 11:23:27 +0200

    ajout de index et du conf bdd

---
+++ db_config.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'db_name' => 'deratech3d_bdd',
+        'db_user' => '359780_deratech3',
+        'db_password' => 'Deratech_3d_Samir',
+        'db_port' => '3306',
+        'db_host' => 'mysql-deratech3d.alwaysdata.net'
+    ];
